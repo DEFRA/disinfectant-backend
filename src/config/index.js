5c4c5bbd7930dfedad6ca3c4bda2f6dfc16eae2b
Aditya Gandhe <aditya.gandhe@defra.gov.uk>
2024-08-22 11:44:51 +0100

    updated changes

--- src/config/index.js
+++ src/config/index.js
@@ -104,7 +104,7 @@ const config = convict({
   disinfectantSchedule: {
     doc: 'How often to poll the disinfectant data (cron format)',
     format: String, // TODO: maybe custom validate this
-    default: '0 0  * * *',
+    default: '0 */60 * * * *',
 
     env: 'DISINFECTANTS__DAILY_SCHEDULE'
   },
