ec705a7c3cb055ae2822c02d8619c97f6d438849
Mizanul Hoque <mizanul.hoque@defra.gov.uk>
2025-12-04 11:53:32 +0000

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -62,14 +62,14 @@ const config = convict({
     format: String,
     nullable: true,
     default: null,
-    env: 'CDP_HTTP_PROXY'
+    env: 'HTTP_PROXY'
   },
   httpsProxy: {
     doc: 'HTTPS Proxy',
     format: String,
     nullable: true,
     default: null,
-    env: 'CDP_HTTPS_PROXY'
+    env: 'HTTP_PROXY'
   },
   azTenantId: {
     doc: 'The azure tenant ID',
